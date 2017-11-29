8f3b43e2d37c0cb30c8bca05a8907a348647d100
Chris Churas <churas@gmail.com>
2017-11-29 13:33:26 -0800

    added mkdir calls and set default gpu to be 0

--- model/inception_residual_train_prediction_3fm/train.sh
+++ model/inception_residual_train_prediction_3fm/train.sh
@@ -1,10 +1,12 @@
 #0!/bin/bash
 caffe_path=../../../caffe_nd_sense_segmetation/.build_release/tools
 log_dir=LOG
+mkdir -p $log_dir
+mkdir -p trained_weights
 GLOG_log_dir=$log_dir $caffe_path/caffe.bin train \
 --solver=solver.prototxt \
---gpu 3 \
---weights=trained_weights/inception_fcn_mscal_classifier_fullstacks_train_iter_44000.caffemodel
+--gpu 0 
+#--weights=trained_weights/inception_fcn_mscal_classifier_fullstacks_train_iter_44000.caffemodel
 #--snapshot=trained_weights/inception_fcn_mscal_classifier_fullstacks_train_iter_1322.solverstate
 #--snapshot=trained_weights/inception_fcn_iter_20069.solverstate \
 #--gpu 3
