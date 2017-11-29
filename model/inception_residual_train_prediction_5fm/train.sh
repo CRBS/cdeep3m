77093d2e3d2b0a459472bea1d0fe7b4c8cfe09b4
Chris Churas <churas@gmail.com>
2017-11-29 13:38:05 -0800

    fixed typo in path to caffe binary

--- model/inception_residual_train_prediction_5fm/train.sh
+++ model/inception_residual_train_prediction_5fm/train.sh
@@ -6,6 +6,8 @@ mkdir -p trained_weights
 GLOG_log_dir=$log_dir $caffe_path/caffe.bin train \
 --solver=solver.prototxt \
 --gpu 0
-#--snapshot=trained_weights/inception_fcn_mscal_classifier_iter_12470.solverstate \
+#--weights=trained_weights/inception_fcn_mscal_classifier_fullstacks_train_iter_44000.caffemodel
+#--snapshot=trained_weights/inception_fcn_mscal_classifier_fullstacks_train_iter_1322.solverstate
+#--snapshot=trained_weights/inception_fcn_iter_20069.solverstate \
 #--gpu 3
 #--weights=../../trained_temp/bigneuron_7fm_vgg_init_deconv_iter_391.caffemodel \
