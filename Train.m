cca34ffba164e97ca9023d2415d89267a080442e
Chris Churas <churas@gmail.com>
2017-12-10 23:49:31 +0000

    figured out how to get name of script being run

--- Train.m
+++ Train.m
@@ -13,26 +13,31 @@
 % ------------------------------------------------------------------------------
 %% Initialize
 % ------------------------------------------------------------------------------
+prog_name = program_name();
+program_invocation_name();
 
-disp('Starting Train');
 addpath(genpath('~/deep3m/scripts/'));
 tic
 pkg load hdf5oct
 pkg load image
 
-arg_list = argv ();
+% suffix for hdf5 files
+H_FIVE_SUFFIX='.h5';
+
+arg_list = argv();
 
 if numel(arg_list)<2; 
-  disp('Use -> Train <Input train data directory> <Output directory>'); 
+  msg = sprintf('%s <Input train data directory> <output dir>\n', prog_name);
+  error(msg); 
   return; 
-end
+endif
 
 in_img_path = make_absolute_filename(arg_list{1});
 
 if isdir(in_img_path) == 0;
   disp('First argument is not a directory and its supposed to be')
   return;
-end
+endif
 
 outdir = make_absolute_filename(arg_list{2});
 
@@ -40,18 +45,26 @@ outdir = make_absolute_filename(arg_list{2});
 % Examine input training data and generate list of h5 files
 % ------------------------------------------------------------------------------
 
-train_files = glob(strcat(in_img_path, filesep(),'*.h5'))
+train_files = glob(strcat(in_img_path, filesep(),'*', H_FIVE_SUFFIX));
 
-if rows(tf) != 16;
-  disp('Expecting 16 .h5 files, but found a different count.')
+if rows(train_files) != 16;
+  fprintf(stderr(),'Expecting 16 .h5 files, but found a different count.\n')
   return;
-end
+endif
 
 % ------------------------------------------------------------------------------
-% Create output directory and copy over model files and adjust configuration files
+% Create output directory and copy over model files and 
+% adjust configuration files
 % ------------------------------------------------------------------------------
 
-
+if isdir(outdir) == 0;
+  mkdir_result = mkdir(outdir)
+  if mkdir_result(1) == 0;
+    fprintf(stderr(),'Error making directory\n');
+    error('yo');
+    return;
+  endif
+endif
 
 % ------------------------------------------------------------------------------
 % Run train 1fm, 3fm, 5fm
