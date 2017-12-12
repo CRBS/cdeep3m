d292c8089919f04b9797d6b835312c108d1e0f93
Chris Churas <churas@gmail.com>
2017-12-12 00:41:38 +0000

    got a complete script here, but without retry and other checks

--- Train.m
+++ Train.m
@@ -14,7 +14,9 @@
 %% Initialize
 % ------------------------------------------------------------------------------
 
-addpath(genpath('~/deep3m/scripts/'));
+script_dir = fileparts(make_absolute_filename(program_invocation_name()));
+
+addpath(genpath(strcat(script_dir,filesep(),'scripts',filesep())));
 tic
 pkg load hdf5oct
 pkg load image
@@ -34,11 +36,11 @@ function create_dir(thedir)
   endif
 endfunction
 
-function copy_model(the_model, dest_dir)
+function copy_model(base_dir, the_model, dest_dir)
   % Copies *txt files from model/inception_residual_train_prediction_<the_model> directory
   % to directory specified by dest_dir argument. If copy fails error() is invoked describing
   % the issue
-  src_files = strcat('./model',filesep(),
+  src_files = strcat(base_dir,filesep(),'model',filesep(),
                      'inception_residual_train_prediction_',the_model,
                      filesep(),'*txt');
   res = copyfile(src_files,dest_dir);
@@ -46,6 +48,68 @@ function copy_model(the_model, dest_dir)
     errmsg = sprintf('Error copying model %s : %s\n',the_model,res(2));
     error(errmsg);
   endif 
+endfunction
+
+function [onefm_dest, threefm_dest, fivefm_dest] = copy_over_allmodels(base_dir, outdir)
+  % ----------------------------------------------------------------------------
+  % Create output directory and copy over model files and 
+  % adjust configuration files
+  % ----------------------------------------------------------------------------
+
+  create_dir(outdir);
+
+  % copy over 1fm, 3fm, and 5fm model data to separate directories
+  onefm_dest = strcat(outdir,filesep(),'1fm');
+  create_dir(onefm_dest);
+  copy_model(base_dir,'1fm',onefm_dest);
+
+  threefm_dest = strcat(outdir,filesep(),'3fm');
+  create_dir(threefm_dest);
+  copy_model(base_dir,'3fm',threefm_dest);
+
+  fivefm_dest = strcat(outdir,filesep(),'5fm');
+  create_dir(fivefm_dest);
+  copy_model(base_dir,'5fm',fivefm_dest);
+endfunction
+
+function [train_model_dest] = update_solverproto_txt_file(outdir,model,max_iterations)
+  % Open solver.prototxt and adjust the line snapshot_prefix to be 
+  % <model>_model/<model>_classifier'
+  solver_prototxt = strcat(outdir,filesep(),model,filesep(), 'solver.prototxt');
+  s_data = fileread(solver_prototxt);
+  solver_out = fopen(solver_prototxt,"w");
+  lines = strsplit(s_data,'\n');
+  model_dir = strcat(outdir,filesep(),model,filesep(),'trainedmodel');
+  create_dir(model_dir);
+  train_model_dest = strcat(model_dir,
+                            filesep(),model,'_classifer');
+  for j = 1:columns(lines)
+    if index(char(lines(j)),'snapshot_prefix:') == 1;
+      fprintf(solver_out,'snapshot_prefix: %s\n',train_model_dest);
+    else
+      if index(char(lines(j)),'max_iter:') == 1;
+        fprintf(solver_out,'max_iter: %d\n',max_iterations); 
+      else
+        fprintf(solver_out,'%s\n',char(lines(j)));
+      endif
+    endif
+  endfor
+  fclose(solver_out);
+endfunction
+
+function update_train_val_prototxt(outdir,model,train_file)
+  % updates data_source in train_val.prototxt file
+  train_val_prototxt = strcat(outdir,filesep(),model,filesep(),'train_val.prototxt');
+  t_data = fileread(train_val_prototxt);
+  lines = strsplit(t_data,'\n');
+  train_out = fopen(train_val_prototxt,"w");
+  for j = 1:columns(lines)
+    if index(char(lines(j)),'data_source:') >= 1;
+      fprintf(train_out,'    data_source: "%s"\n',train_file);
+    else
+      fprintf(train_out,'%s\n',char(lines(j)));
+    endif
+  endfor
 endfunction
 
 
@@ -56,9 +120,12 @@ function runtrain(arg_list)
   % then copying over models under model/ directory to output directory
   % suffix for hdf5 files
   H_FIVE_SUFFIX='.h5';
-
   prog_name = program_name();
-  program_invocation_name();
+  base_dir = fileparts(make_absolute_filename(program_invocation_name()));
+  
+  caffe_train_template=strcat(base_dir,filesep(),'scripts',filesep(),'caffetrain_template.sh');
+  caffe_bin='/home/ubuntu/caffe_nd_sense_segmentation/build/tools/';
+
   if numel(arg_list)<2; 
     msg = sprintf('%s <Input train data directory> <output dir>\n', prog_name);
     error(msg); 
@@ -81,34 +148,47 @@ function runtrain(arg_list)
   train_files = glob(strcat(in_img_path, filesep(),'*', H_FIVE_SUFFIX));
 
   if rows(train_files) != 16;
-    fprintf(stderr(),'Expecting 16 .h5 files, but found a different count.\n')
+    fprintf(stderr(),'Expecting 16 .h5 files, but found a different count.\n');
     return;
+  endif
+
+  train_file = strcat(in_img_path,filesep(),'train_file.txt');
+  if ~exist(train_file);
+    errmsg = sprintf('%s file not found',train_file);
+    error(errmsg);
   endif
 
   % ----------------------------------------------------------------------------
   % Create output directory and copy over model files and 
   % adjust configuration files
   % ----------------------------------------------------------------------------
+  [onefm_dest,threefm_dest,fivefm_dest] = copy_over_allmodels(base_dir,outdir);
+  max_iterations = 10000;
+  update_solverproto_txt_file(outdir,'1fm',max_iterations);
+  update_solverproto_txt_file(outdir,'3fm',max_iterations);
+  update_solverproto_txt_file(outdir,'5fm',max_iterations);
 
-  create_dir(outdir);
-
-  % copy over 1fm, 3fm, and 5fm model data to separate directories
-  onefm_dest = strcat(outdir,filesep(),'1fm');
-  create_dir(onefm_dest);
-  
-  threefm_dest = strcat(outdir,filesep(),'3fm');
-  create_dir(threefm_dest);
-
-  fivefm_dest = strcat(outdir,filesep(),'5fm');
-  create_dir(fivefm_dest);
-  
-  copy_model('1fm',onefm_dest);
-  copy_model('3fm',threefm_dest);
-  copy_model('5fm',fivefm_dest);
-
+  update_train_val_prototxt(outdir,'1fm',train_file);
+  update_train_val_prototxt(outdir,'3fm',train_file);
+  update_train_val_prototxt(outdir,'5fm',train_file);
+  caffe_train = strcat(outdir,filesep(),'caffe_train.sh');
+  copyfile(caffe_train_template,caffe_train);
   % ----------------------------------------------------------------------------
   % Run train 1fm, 3fm, 5fm
   % ----------------------------------------------------------------------------
+  fprintf(stdout(),'Running Training for 1fm\n');
+
+  
+  models = {"1fm","3fm","5fm"};
+  for i = 1:columns(models)
+    the_cmd = sprintf('%s %s %s %s\n',caffe_train,char(models(i)),caffe_bin,'all');
+    res = system(the_cmd);
+    if res ~= 0;
+      fprintf(stderr(),'%s training exited with non zero exit code: %d\n',char(models(i)),
+              res);
+    endif
+  endfor
+  
 endfunction
 
 runtrain(argv());
