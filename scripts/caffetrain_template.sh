cf34cf2a54a0a4496de2a9bb530c80ba7c185484
Chris Churas <churas@gmail.com>
2017-12-12 12:30:50 -0800

    improved template script by adding better documentation and code to add ending slash to caffe path if needed

--- scripts/caffetrain_template.sh
+++ scripts/caffetrain_template.sh
@@ -3,16 +3,30 @@
 caffe_path=""
 
 if [ $# -ne 3 ] ; then
-  echo "$0 <model ie 1fm, 3fm, or 5fm> <caffe bin path with ending /> <which gpu to use 'all' for all or # ie 1>"
+  echo "$0 <model> <caffe bin path> <gpu>"
   echo ""
-  echo "Runs caffe on model specified as first argument"
+  echo "Runs caffe on model specified by first argument. The trained"
+  echo "model will be stored in <model>/trainedmodel directory"
+  echo "Output from caffe will be redirected to <mode>/log/out.log"
+  echo ""
+  echo "<model> -- The model to train, should be one of the following:"
+  echo "           1fm, 3fm, or 5fm"
+  echo ""
+  echo "<caffe bin path> -- Directory where caffe.bin binary resides"
+  echo "                    If no path needed speicify \"\""
+  echo ""
+  echo "<gpu> -- The gpu to use (expects a number ie 0, or 1 or all)"
   echo ""
   exit 1
 fi
 
 script_dir=`dirname "$0"`
 model=$1
-caffe_path=$2
+
+if [ "$2" != "" ] ; then
+   caffe_path="${2}/"
+fi
+
 gpu=$3
 
 base_dir=`cd "$script_dir";pwd`
@@ -34,8 +48,16 @@ snapshot_opts=""
 if [ ! "$latest_iteration" == "" ] ; then
   snap_file=`find "$model_dir/trainedmodel" -name "*${latest_iteration}.solverstate" -type f`
   snapshot_opts="--snapshot=$snap_file"
+  echo "Resuming run from snapshot file: $snap_file"
 fi
 
 pushd "$model_dir"
-GLOG_log_dir=$log_dir $caffe_path/caffe.bin train --solver=$model_dir/solver.prototxt --gpu $gpu $snapshot_opts
+GLOG_log_dir=$log_dir $caffe_path/caffe.bin train --solver=$model_dir/solver.prototxt --gpu $gpu $snapshot_opts > "${model_dir}/log/out.log" 2>&1
+exitcode=$?
 popd
+
+if [ $exitcode != 0 ] ; then
+  echo "ERROR: caffe had a non zero exit code: $exitcode"
+fi
+
+exit $exitcode
