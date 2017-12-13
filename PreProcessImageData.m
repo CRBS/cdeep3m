b6d0dc15bb239f61589476087e73fcd8aa6fbfc9
haberlmatt <haberlmatt@gmail.com>
2017-12-12 17:59:20 -0800

    Update PreProcessImageData.m

--- PreProcessImageData.m
+++ PreProcessImageData.m
@@ -27,16 +27,17 @@ if numel(arg_list)<2; disp('Use -> PreProcessImageData /ImageData/EMdata1/ /Imag
 in_img_path = arg_list{1};
 outdir = arg_list{2};
 
+
 % ----------------------------------------------------------------------------------------
 %% Load image data / Check if large data first
 % ----------------------------------------------------------------------------------------
 
 imagesize = check_image_size(in_img_path);
-packages = break_large_img(imagesize);
+[packages,z_blocks] = break_large_img(imagesize);
 num_of_pkg = numel(packages);
 %num_of_pkg =1;  %Change here to enable large image files
 
-if num_of_pkg ==1
+if num_of_pkg==1 && numel(z_blocks)==2
     [d_tr] = imageimporter(in_img_path);
     checkpoint_nobinary(d_tr);
     [d_tr] = add_z_padding(d_tr); %adds 2 planes i beginning and end
@@ -54,44 +55,53 @@ if num_of_pkg ==1
     end
     
     
-elseif num_of_pkg > 1
-    for ii = 1:num_of_pkg
-        if ii ==1; t1 = tic; end
-        if ii ==2; t_int = toc(t1)/60; end
-        fprintf('------- Image Augmentation large data ------\n');
-        fprintf('-------- Augmenting Part %s out of %s -------\n', num2str(ii), num2str(num_of_pkg));
-        if ii>2
-        fprintf('-> Remaining time estimated: %s min\n', num2str(round(t_int*(num_of_pkg-ii))));
-        end
-        %define label name
-        area = packages{ii};
-        [d_tr] = imageimporter_large(in_img_path,area); %load only subarea here
-        checkpoint_nobinary(d_tr);
-        [d_tr] = add_z_padding(d_tr); %adds 2 planes i beginning and end
-        d_tr=permute(d_tr,[3 1 2]); %from tiff to h5 /xyz to z*x*y
-        %% Save image data
-        d_details = '/data';
-        %%augment_and_save
-        [data]=augment_image_data_only(d_tr);
-        
-        outsubdir = fullfile(outdir, sprintf('Pkg_%03', ii));
-        if ~exist(outsubdir,'dir'), mkdir(outsubdir); end
-        disp('Saving Hd5 files')
-        for i=1:length(data)
-            subdata=data{i};
-            filename = fullfile(outsubdir, sprintf('test_data_full_stacks_v%s.h5', num2str(i)));
-            %h5create(filename,d_details,size(subdata)); %nescessary for Matlab not for Octave
-            h5write(filename,d_details,subdata);
+else
+    
+    for zz = 1:numel(z_blocks)-1
+        if zz == 1
+            z_stack = [z_blocks(zz), z_blocks(zz+1)]
+        else
+            z_stack = [z_blocks(zz)+1, z_blocks(zz+1)]
         end
         
-        if ii ==num_of_pkg
-        %save(fullfile(outdir,'packages.mat'),'packages');
-        save(fullfile(outdir,'de_augmentation_info.mat'),'packages','num_of_pkg','imagesize');
-        % To do: Write a predict file that runs through all folders in outdir
-        % and write a merge file for postprocessing in outdir
-        end
-
-    end
+        
+        for ii = 1:num_of_pkg
+            if ii ==1; t1 = tic; end
+            if ii ==2; t_int = toc(t1)/60; end
+            fprintf('------- Image Augmentation large data ------\n');
+            fprintf('-------- Augmenting Part %s out of %s -------\n', num2str(ii), num2str(num_of_pkg));
+            if ii>2
+                fprintf('-> Remaining time estimated: %s min\n', num2str(round(t_int*(num_of_pkg-ii))));
+            end
+            %define label name
+            area = packages{ii};
+            [d_tr] = imageimporter_large(in_img_path,area,z_stack); %load only subarea here
+            checkpoint_nobinary(d_tr);
+            [d_tr] = add_z_padding(d_tr); %adds 2 planes i beginning and end
+            d_tr=permute(d_tr,[3 1 2]); %from tiff to h5 /xyz to z*x*y
+            %% Save image data
+            d_details = '/data';
+            %%augment_and_save
+            [data]=augment_image_data_only(d_tr);
+            
+            outsubdir = fullfile(outdir, sprintf('Pkg%03d_Z%02d',ii, zz));
+            if ~exist(outsubdir,'dir'), mkdir(outsubdir); end
+            disp('Saving Hd5 files')
+            for i=1:length(data)
+                subdata=data{i};
+                filename = fullfile(outsubdir, sprintf('test_data_full_stacks_v%s.h5', num2str(i)));
+                h5create(filename,d_details,size(subdata)); %nescessary for Matlab not for Octave
+                h5write(filename,d_details,subdata);
+            end
+            
+            if ii ==num_of_pkg
+                %save(fullfile(outdir,'packages.mat'),'packages');
+                save(fullfile(outdir,'de_augmentation_info.mat'),'packages','num_of_pkg','imagesize');
+                % To do: Write a predict file that runs through all folders in outdir
+                % and write a merge file for postprocessing in outdir
+            end
+        end % pkgs
+    end %z-block
 end
 
 % ----------------------------------------------------------------------------------------
@@ -101,6 +111,6 @@ end
 disp('Image Augmentation completed');
 toc
 if exist('txts.save','file')
-load('txts.save','-mat','image_info_text');
-fprintf(image_info_text);
+    load('txts.save','-mat','image_info_text');
+    fprintf(image_info_text);
 end
