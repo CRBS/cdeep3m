87d49c937e65ba7e8a52c2943467e17aa902c4b9
haberlmatt <haberlmatt@gmail.com>
2017-12-12 17:58:35 -0800

    Update imageimporter_large.m

--- scripts/functions/imageimporter_large.m
+++ scripts/functions/imageimporter_large.m
@@ -1,4 +1,4 @@
-function [imgstack] = imageimporter_large(img_path,area)
+function [imgstack] = imageimporter_large(img_path,area,z_stack)
 %imageimporter_large: loads subarea of large image data 
 % from folder or from an individual image stack
 %
@@ -13,20 +13,22 @@ disp(img_path);
 [Dir,name,ext] = fileparts(img_path);
 rows = [area(1), area(2)];
 cols = [area(3), area(4)];
+all_zs =  [z_stack(1):z_stack(2)];
 if ~isempty(ext)
     if ~isempty(strfind(ext,'h5'))
         fprintf('Reading H5 image file %s\n', img_path);
         hinfo = h5info(img_path);
-        start = [1, rows(1), cols(1)];
-        count = [Inf, rows(2)-rows(1), cols(2)-cols(1)];
+        start = [z_stack(1), rows(1), cols(1)];
+        count = [z_stack(2), rows(2)-rows(1), cols(2)-cols(1)];
         %h5read(img_path, hinfo.GroupHierarchy.Datasets.Name);  
         imgstack = h5read(img_path, ['/', hinfo.Datasets.Name],start,count);
         imgstack=permute(imgstack,[2 3 1]); %To match the same format as TIF or PNG images
     elseif ~isempty(strfind(ext,'tif'))
         info = imfinfo(img_path);
-        fprintf('Reading image stack with %d images\n',size(info,1));
-        for idx =1:size(info,1)
-            imgstack(:,:,idx) = imread(img_path,'index',idx,'PixelRegion', {rows, cols});
+        fprintf('Reading %s image stack with %d images\n', num2str(z_stack(2)-z_stack(1)),size(info,1));
+        for iii =1:numel(all_zs)
+            idx = all_zs(iii);
+            imgstack(:,:,iii) = imread(img_path,'index',idx,'PixelRegion', {rows, cols});
         end
         
     end
@@ -39,14 +41,15 @@ elseif isdir(img_path)
     else
         [~, type] = max([size(tif_list,1),size(png_list,1)]); %only read tif or pngs if ambiguous
         if type==1, file_list = tif_list; elseif type==2, file_list = png_list; end
-        for idx =1:size(file_list,1)
+        for iii =1:numel(all_zs)
+            idx = all_zs(iii);
             filename = fullfile(img_path,file_list(idx).name);
             fprintf('Reading file: %s\n', filename); 
             if type==1
-            imgstack(:,:,idx) = imread(filename,'PixelRegion', {rows, cols}); %tif allows to read subarea
+            imgstack(:,:,iii) = imread(filename,'PixelRegion', {rows, cols}); %tif allows to read subarea
             else
             rect = [area(1), area(3), area(2)-area(1), area(4)-area(3)];
-            imgstack(:,:,idx) = imcrop(imread(filename),rect);    %otherwise need to crop the area here
+            imgstack(:,:,iii) = imcrop(imread(filename),rect);    %otherwise need to crop the area here
             end
         end
     end
@@ -57,4 +60,5 @@ else
 end
 
 end
+
 
