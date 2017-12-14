618019c26eb9115061f44596e3b41bed5db5cde5
Chris Churas <churas@gmail.com>
2017-12-14 23:29:35 +0000

    forgot to fix function name in tests, had it set to _folders instead of _names

--- scripts/functions/get_train_basemodel_names.m
+++ scripts/functions/get_train_basemodel_names.m
@@ -1,10 +1,10 @@
-## Usage [pkgfolders] = get_train_basemodel_folders ( thedir )
+## Usage [pkgfolders] = get_train_basemodel_names ( thedir )
 ##
 ## Returns list of of *fm directories in thedir passed in.
 ## If there is an error creating directory then error() is
 ## invoked with message
 
-function [trainfolders] = get_train_basemodel_folders(thedir)
+function [trainfolders] = get_train_basemodel_names(thedir)
   if isdir(thedir) == 0;
     errmsg = sprintf('%s is not a directory',thedir);
     error(errmsg);
@@ -23,35 +23,35 @@ function [trainfolders] = get_train_basemodel_folders(thedir)
   endfor
 endfunction
 
-%!error <is not a directory> get_train_basemodel_folders('');
+%!error <is not a directory> get_train_basemodel_names('');
 
-%!error <is not a directory> get_train_basemodel_folders(program_invocation_name());
+%!error <is not a directory> get_train_basemodel_names(program_invocation_name());
 
 %!test
 %! test_fname = tempname();
 %! mkdir(test_fname);
-%! [res] = get_train_basemodel_folders(test_fname);
+%! [res] = get_train_basemodel_names(test_fname);
 %! assert(columns(res) == 0);
 %! one_fm = strcat(test_fname,filesep(),'1fm');
 %! mkdir(one_fm);
-%! [res] = get_train_basemodel_folders(test_fname);
+%! [res] = get_train_basemodel_names(test_fname);
 %! assert(rows(res) == 1);
 %! assert(char(res(1)) == '1fm');
 %! three_fm = strcat(test_fname,filesep(),'3fm');
 %! mkdir(three_fm);
-%! [res] = get_train_basemodel_folders(test_fname);
+%! [res] = get_train_basemodel_names(test_fname);
 %! assert(rows(res) == 2);
 %! de_aug_file = strcat(test_fname,filesep(),'de_augmentation_info.mat');
 %! f_out = fopen(de_aug_file,'w');
 %! fprintf(f_out,'hi\n');
 %! fclose(f_out);
-%! [res] = get_train_basemodel_folders(test_fname);
+%! [res] = get_train_basemodel_names(test_fname);
 %! assert(rows(res) == 2);
 %! pkg_file = strcat(test_fname,filesep(),'6fm');
 %! f_out = fopen(pkg_file,'w');
 %! fprintf(f_out,'hi\n');
 %! fclose(f_out);
-%! [res] = get_train_basemodel_folders(test_fname);
+%! [res] = get_train_basemodel_names(test_fname);
 %! assert(rows(res) == 2);
 %! rmdir(test_fname,'s');
 
