e4ad340a39f64a59c9dfff7e586294c1868908e5
haberlmatt <haberlmatt@gmail.com>
2017-12-18 15:59:26 -0800

    Update filter_files.m

--- scripts/functions/filter_files.m
+++ scripts/functions/filter_files.m
@@ -2,12 +2,14 @@ function list = filter_files(list,fileformat)
 %filter_files
 % Reducing the factors that are not the 'fileformat' in 'list' 
 for i=1:length(list)
-list(i).fileformat = strfind(list(i).name,fileformat);
+clear -v ext
+[~,~,ext] = fileparts(list(i).name);
+list(i).fileformat = strcmpi(ext,fileformat);
 if isempty(list(i).fileformat)
     list(i).fileformat = 0;
-end    
-fileformats(i) = double(list(i).fileformat);
+end   
+fileformats(i) = list(i).fileformat;
 end
-fileformats_in_folder = find(fileformat);
+fileformats_in_folder = find(fileformats);
 list = list(fileformats_in_folder);
 end
