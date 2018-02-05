1be0d47c07393f5dfe22bd5197ede93d90a27bb4
haberlmatt <haberlmatt@gmail.com>
2018-02-05 14:58:33 -0800

    Update StartPostprocessing.m

--- StartPostprocessing.m
+++ StartPostprocessing.m
@@ -1,10 +1,8 @@
 #!/usr/bin/octave -qf
 % New Postprocessing
-% Syntax: StartPostprocessing /example/seg1/predict/ outputfilename /example/seg2/predict/ outputfilename2
-% You can call a subsequent script with the last command e.g. AndWatershed
-% In this case all predictions will be merged and used for Watershed
-% subsequently
-% Runtime 3min for 1024x1024x100 dataset
+% Syntax: StartPostprocessing /example/seg1/predict/ /example/seg2/predict/
+%
+% Runtime estimate 2min for 1024x1024x100 dataset
 %
 %------------------------------------------------------------------
 %% New -- NCMIR/NBCR, UCSD -- Author: M Haberl -- Date: 10/2017
@@ -15,50 +13,35 @@ addpath(genpath('/home/ubuntu/deep3m/scripts/'));
 pkg load hdf5oct
 pkg load image
 
-if ~numel(arg_list) >= 2
-  disp('Please specify at least 1 input directory and 1 outputname');  
-  disp('Use -> StartPostprocessing /example/seg1/predict/ outputfilename /example/seg2/predict/ outputfilename2'); 
+if ~numel(arg_list) >= 1
+  disp('Please specify at least 1 input directory');  
+  disp('Use -> StartPostprocessing /example/seg1/predict/ /example/seg2/predict/'); 
 return 
 end
 tic
 
 %% Enable batch processing all predictions
 disp('Starting to merge de-augment data');
-fprintf('Starting to process %d datasets \n',floor(numel(arg_list)/2));
-for i = 1:floor(numel(arg_list)/2)
-inputdir = arg_list{(2*i)-1};
-outputname = arg_list{(2*i)};
+fprintf('Starting to process %d datasets \n',(numel(arg_list)));
+for i = 1:floor(numel(arg_list))
+inputdir = arg_list{i};
 
 if ~isdir(inputdir)
   error(sprintf('%s not a input directory',inputdir));
   return
 end
 
-
-%addpath(genpath('./PostProcessing'))
-%load("-ascii",filename);
-%addpath(fullfile(program_dir,'scripts'));
-
 fprintf('Generating Average Prediction of %s\n',inputdir)
 average_prob_folder = merge_16_probs_v2(inputdir);
 
-
-%save_mat_file=fullfile(inputdir,['ave_probs_',outputname, '.mat']);
-%save_h5_file=fullfile(inputdir, ['ave_probs_',outputname,'.h5']);
-%save(save_mat_file,'average','-V7');
-%p=single(average);
-%d_details = '/probabilities';
-%hdf5write(save_h5_file,d_details,p);
-
 end
 fprintf('Elapsed runtime for data-deaugmentation: %04d seconds.\n', round(toc));
-tic
 
 %% Run Merge Predictions now
 %MergePredictions 
 
 %% Run 3D Watershed if required
-if regexpi(arg_list{end},'water','once')
-readvars =1;   
-Run_3DWatershed_onPredictions
-end
+%if regexpi(arg_list{end},'water','once')
+%readvars =1;   
+%Run_3DWatershed_onPredictions
+%end
