1cedd94a28057aab4dd5e7d39b24494acfca36b8
haberlmatt <haberlmatt@gmail.com>
2018-02-05 12:51:43 -0800

    PreProcessTrainingData faster now

--- PreprocessTrainingData.m
+++ PreprocessTrainingData.m
@@ -10,7 +10,8 @@
 %% PreprocessTraining for Deep3M -- NCMIR/NBCR, UCSD -- Author: M Haberl -- Date: 10/2017
 %----------------------------------------------------------------------------------------
 %
-% Runtime ~20min for 1024x1024x100 dataset
+% Adapted to speed up time
+% reduced Runtime from >20min for 1024x1024x100 dataset to ~1-2 min
 %
 
 
@@ -40,57 +41,76 @@ outdir = arg_list{3};
 disp('Output Path:');disp(outdir); 
 
 % ----------------------------------------------------------------------------------------
-%% Load train data
-% ----------------------------------------------------------------------------------------
-
-disp('Loading:');
-disp(label_img_path); 
-[lblstack] = imageimporter(label_img_path);
-checkpoint_isbinary(lblstack);
-
-% ----------------------------------------------------------------------------------------
 %% Load training images
 % ----------------------------------------------------------------------------------------
 
 disp('Loading:');
-disp(trainig_img_path); 
+disp(trainig_img_path);
 [imgstack] = imageimporter(trainig_img_path);
+disp('Verifying images');
 checkpoint_nobinary(imgstack);
+
+% ----------------------------------------------------------------------------------------
+%% Load train data
+% ----------------------------------------------------------------------------------------
+
+disp('Loading:');
+disp(label_img_path);
+[lblstack] = imageimporter(label_img_path);
+disp('Verifying labels');
+checkpoint_isbinary(lblstack);
 
 % ----------------------------------------------------------------------------------------
 %% Augment the data, generating 16 versions and save
 % ----------------------------------------------------------------------------------------
 
 %imshow(labels_arr(:,:,1))
-disp('Augmenting ...');
-data_arr=permute(imgstack,[3 1 2]); %from tiff to h5 /100*1000*1000
-labels_arr=permute(lblstack,[3 1 2]); %from tiff to h5 /100*1000*1000
-
-d_tr =single(data_arr);
-l_tr =single(labels_arr);
-[data,labels]=augment_data(d_tr,l_tr); 
-
+%data_arr=permute(imgstack,[3 1 2]); %from tiff to h5 /100*1000*1000
+%labels_arr=permute(lblstack,[3 1 2]); %from tiff to h5 /100*1000*1000
 %[outdir,name,ext] = fileparts(save_file);
 
-d_details = '/data'; 
-l_details = '/label'; 
+img_v1 =single(imgstack);
+lb_v1 =single(lblstack);
+
+d_details = '/data';
+l_details = '/label';
 if ~exist(outdir,'dir'), mkdir(outdir); end
 ext = '.h5';
+
+disp('Augmenting training data 1-8 and 9-16'); 
+for i=1:8
+    %% v1-8
+    [img,lb]=augment_data(img_v1,lb_v1,i);
     
-disp('Saving ...');
-for i=1:length(data)
-    d_tr=data{i};
-    l_tr=labels{i};
+    %% v9-16
+    inv_img = flip(img,3); %var 9 -16
+    inv_lb = flip(lb,3);  %var 9 -16
+    
+    %% v1-8
+    img=permute(img,[3 1 2]); %from tiff to h5 /100*1000*1000
+    lb=permute(lb,[3 1 2]); %from tiff to h5 /100*1000*1000
     filename = fullfile(outdir, sprintf('training_full_stacks_v%s%s', num2str(i), ext));
-    disp(filename); 
-    h5write(filename,d_details,d_tr);
-    h5write(filename,l_details,l_tr); 
+    fprintf('Saving: %s\n', filename);
+    %h5write(filename,d_details,img);
+    %h5write(filename,l_details,lb);
+    clear img lb
+    %% v9-16
+    inv_img = permute(inv_img,[3 1 2]); %from tiff to h5 /100*1000*1000
+    inv_lb  = permute(inv_lb,[3 1 2]);  %from tiff to h5 /100*1000*1000
+    filename = fullfile(outdir, sprintf('training_full_stacks_v%s%s', num2str(i+8), ext));
+     fprintf('Saving: %s\n', filename);
+    %h5write(filename,d_details,inv_img);
+    %h5write(filename,l_details,inv_lb); 
+    clear inv_img inv_lb
 end
+
 
 % ----------------------------------------------------------------------------------------
 %% Completed
 % ----------------------------------------------------------------------------------------
 
-disp('Image Augmentation completed');
 toc
+disp('-> Training data augmentation completed');
+fprintf('Training data stored in %s\n', outdir);
+%disp('For training your model please run Train.m');
 fprintf('For training your model please run ./CreateTrainJob.m %s < ~/outputdirectory >\n', outdir);
