71f0ea3b084efe467af2aa5a30e01ddc9aad1cdf
Chris Churas <churas.camera@gmail.com>
2018-02-07 11:11:32 -0800

    updated to download correct version of hdf5oct

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,12 +1,11 @@
 
 apt-get -y upgrade
 apt-get -y update
-apt-get -y install octave octave-image octave-pkg-dev
+apt-get -y install octave octave-image octave-pkg-dev git python-pip unzip
 
 cd ~
-wget https://github.com/stegro/hdf5oct/archive/0.4.0.tar.gz
-tar -zxf 0.4.0.tar.gz
-cd hdf5oct-0.4.0
-
+wget https://github.com/stegro/hdf5oct/archive/b047e6e611e874b02740e7465f5d139e74f9765f.zip
+unzip b047e6e611e874b02740e7465f5d139e74f9765f.zip
+cd hdf5oct-*
 make
-make install 
+make install
