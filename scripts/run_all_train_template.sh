32690149730f38068fe7bc78bb6b752f190142ea
Chris Churas <churas.camera@gmail.com>
2018-02-14 21:29:55 +0000

    added optional parameters --gpu --numiterations --1fmonly

--- scripts/run_all_train_template.sh
+++ scripts/run_all_train_template.sh
@@ -1,21 +1,50 @@
 #!/bin/bash
 
-if [ $# -ne 1 ] ; then
-  echo "$0 <# iterations>"
-  echo ""
-  echo "Runs caffe_train.sh for all three models in directory this script is located"
-  echo ""
-  echo "<# iterations> -- Sets number of iterations caffe should be run."
-  echo "                  Ex: 1000 for 1,000 iterations"
-  echo ""
-  exit 1
+script_name=`basename $0`
+script_dir=`dirname $0`
+version="???"
+
+if [ -f "$script_dir/VERSION" ] ; then
+   version=`cat $script_dir/VERSION`
 fi
 
-script_dir=`dirname "$0"`
+numiterations="2000"
+gpu="all"
+function usage()
+{
+    echo "usage: $script_name [-h] [--1fmonly] [--numiterations NUMITERATIONS]
 
-num_iterations=$1
+              Version: $version
 
-time_est=`perl -e "printf('%.2f',${num_iterations}*4/3600);"`
+              Runs caffe training on Deep3M model in directory where
+              this script resides 
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --1fmonly            Only train 1fm model
+  --numiterations      Number of training iterations to run (default $numiterations)
+  --gpu                Which GPU to use, can be a number ie 0 or 1 or
+                       all to use all GPUs (default $gpu)
+
+    " 1>&2;
+   exit 1;
+}
+
+TEMP=`getopt -o h --long "1fmonly,numiterations:" -n '$0' -- "$@"`
+eval set -- "$TEMP"
+
+while true ; do
+    case "$1" in
+        -h ) usage ;;
+        --1fmonly ) one_fmonly=true ; shift ;;
+        --numiterations ) numiterations=$2 ; shift 2 ;;
+        --gpu ) gpu=$2 shift 2 ;;
+        --) break ;;
+    esac
+done
+
+
+time_est=`perl -e "printf('%.2f',${num_iterations}*8/3600);"`
 
 echo ""
 echo "Estimating $time_est hours of processing per model (there are 3 models) on p3.2xlarge"
@@ -23,7 +52,7 @@ echo ""
 
 for Y in `echo 1fm 3fm 5fm` ; do
   echo "Running $Y train, expect this model to train for $time_est hours on p3.2xlarge"
-  /usr/bin/time -p $script_dir/caffe_train.sh $Y $num_iterations all
+  /usr/bin/time -p $script_dir/caffe_train.sh $Y $numiterations $gpu
   if [ $? != 0 ] ; then
     echo "Non zero exit code from caffe for train of $Y model. Exiting."
     outfile="$script_dir/$Y/log/out.log"
@@ -33,6 +62,10 @@ for Y in `echo 1fm 3fm 5fm` ; do
       tail $outfile
     fi
     exit 1
+  fi
+  if [ $one_fmonly == true ] ; then
+    echo "--1fmonly flag set, skipping 3fm and 5fm models."
+    break
   fi
 done
 
