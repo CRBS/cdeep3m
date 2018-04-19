13c8e2a8cc92bd8626592984596672638d44bfcf
haberlmatt <haberlmatt@gmail.com>
2018-04-19 10:53:21 -0700

    Create def_datapackages.m prototype

---
+++ scripts/def_datapackages.m
@@ -0,0 +1,43 @@
+#!/usr/bin/octave -qf
+% def_datapackages
+%
+% -> Defines size of datapackages used for augmentation of image data
+% Input: Image folder and output directory to store de_augmentation file
+% Output: de_augmentation_info.mat
+%
+% Syntax : def_datapackages /ImageData/EMdata1/ /ImageData/AugmentedEMData/
+%
+%
+%----------------------------------------------------------------------------------------
+%% PreProcessImageData for CDeep3M -- NCMIR/NBCR, UCSD -- Author: M Haberl -- Date: 11/2017
+%----------------------------------------------------------------------------------------
+%
+% ----------------------------------------------------------------------------------------
+%% Initialize
+% ----------------------------------------------------------------------------------------
+
+disp('Starting Image Augmentation');
+tic
+script_dir = fileparts(make_absolute_filename(program_invocation_name()));
+addpath(genpath(script_dir));
+addpath(genpath(strcat(script_dir,filesep(),'scripts',filesep())));
+addpath(genpath(strcat(script_dir,filesep(),'scripts',filesep(),'functions')));
+
+
+pkg load hdf5oct
+pkg load image
+
+arg_list = argv ();
+if numel(arg_list)<2; disp('Use -> PreProcessImageData /ImageData/EMdata1/ /ImageData/AugmentedEMData/'); return; end
+in_img_path = arg_list{1};
+outdir = arg_list{2};
+if ~exist(outdir,'dir'), mkdir(outdir); end
+
+% ----------------------------------------------------------------------------------------
+%% 
+% ----------------------------------------------------------------------------------------
+
+imagesize = check_image_size(in_img_path);
+[packages,z_blocks] = break_large_img(imagesize);
+num_of_pkg = numel(packages);
+save(fullfile(outdir,'de_augmentation_info.mat'),'packages','num_of_pkg','imagesize','z_blocks');
