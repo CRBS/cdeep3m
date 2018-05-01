d1020b1116e6e1579bc3a77e099fdd6d9bed44b9
Chris Churas <churas.camera@gmail.com>
2018-05-01 10:31:38 -0700

    updated function description and fixed test to work with new valid_file parameter

--- scripts/functions/update_train_val_prototxt.m
+++ scripts/functions/update_train_val_prototxt.m
@@ -1,7 +1,12 @@
 ## Usage update_train_val_prototxt(outdir, model, train_file, valid_file)
 ##
 ## Updates <outdir>/<model>/train_val.prototxt file
-## replacing path in data_source: line with <train_file>
+## replacing lines with 'train_file.txt' with value of train_file parameter
+## like so:
+## data_source: "train_file"
+## and replacing lines with 'valid_file.txt' with value of valid_file parameter
+## like so:
+## data_source: "valid_file"
 ##
 
 function update_train_val_prototxt(outdir,model,train_file,valid_file)
@@ -33,12 +38,13 @@ endfunction
 %! create_dir(strcat(test_fname,filesep(),model));
 %! trainv = strcat(test_fname,filesep(),model,filesep(),'train_val.prototxt');
 %! f_out = fopen(trainv,'w');
-%! fprintf(f_out,'hi\nThe\ntest_interval: 20\n    data_source: "hi"\n');
+%! fprintf(f_out,'hi\nThe\ntest_interval: 20\n    data_source: "train_file.txt"\n    data_source: "valid_file.txt"\n');
 %! fclose(f_out);
-%! update_train_val_prototxt(test_fname,model,'boo');
+%! update_train_val_prototxt(test_fname,model,'boo', 'haa');
 %! t_data = fileread(trainv);
 %! lines = strsplit(t_data,'\n');
 %! assert(char(lines(4)) == '    data_source: "boo"')
+%! assert(char(lines(5)) == '    data_source: "haa"')
 %! assert(char(lines(2)) == 'The');
 %! rmdir(test_fname,'s');
 
