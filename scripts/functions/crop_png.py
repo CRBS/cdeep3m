0e22f9fe567291479ef8402a705723e7c3977437
chris churas <churas@gmail.com>
2018-05-30 17:14:38 +0000

    added argparse argument parsing

--- scripts/functions/crop_png.py
+++ scripts/functions/crop_png.py
@@ -3,47 +3,61 @@
 """
 Crop image Frame
 """
-
-import cv2
 import sys
 import os
+import argparse
+import cv2
 from joblib import Parallel, delayed
-
-
-inputarg = (sys.argv)
-print 'Number of arguments:', len(inputarg), 'arguments.'
-
-from joblib import Parallel, delayed
-
 from multiprocessing import Pool, TimeoutError
 import time
-import os
 
-inputarg = (sys.argv)
-#print 'Number of arguments:', len(inputarg), 'arguments.'
-in1 = int(inputarg[3])
-in2 = int(inputarg[4])
-in3 = int(inputarg[5])
-in4 = int(inputarg[6])
-print(in1)
-print(in2)
-print(in3)
-print(in4)
-#print 'Argument List:', str(inputarg[2])
-            
-tempmat_infile = str(inputarg[1])
-#print 'Opening:', tempmatfile, '...'
+def _parse_arguments(desc, theargs):
+    """Parses command line arguments using argparse
+    """
+    help_formatter = argparse.RawDescriptionHelpFormatter
+    parser = argparse.ArgumentParser(description=desc,
+                                     formatter_class=help_formatter)
+    parser.add_argument('inputlistfile',
+                        help='File containing list of input image paths')
+    parser.add_argument('outputlistfile',
+                        help='File containing list of output image paths')
+    parser.add_argument('leftxcoord', type=int, help='Left x pixel coordinate')
+    parser.add_argument('rightxcoord', type=int,
+                        help='Right x pixel coordinate')
+    parser.add_argument('topycoord', type=int, help='Top y pixel coordinate')
+    parser.add_argument('bottomycoord', type=int,
+                        help='Bottom y pixel coordinate')
+    return parser.parse_args(theargs)
 
-tempmat_outfile = str(inputarg[2])
-#print 'Opening:', tempmatfile, '...'
+desc = """
+Given a file with a list of images (inputlistfile 1st arg), 
+this program will extract a subimage at coordinates specified
+on the command line and save that subimage to the file on the
+same line in the file with list of output images 
+(outputlistfile 2nd arg) 
+"""
 
-file = open(tempmat_infile, "r")
+# Parse arguments
+theargs = _parse_arguments(desc, sys.argv[1:])
+
+in1 = theargs.leftxcoord
+in2 = theargs.rightxcoord
+in3 = theargs.topycoord
+in4 = theargs.bottomycoord
+
+sys.stdout.write(str(in1) + '\n')
+sys.stdout.write(str(in2) + '\n')
+sys.stdout.write(str(in3) + '\n')
+sys.stdout.write(str(in4) + '\n')
+
+file = open(theargs.inputlistfile, "r")
 lines = [line.rstrip('\n') for line in file]
 file.close()
 
-file = open(tempmat_outfile, "r")
+file = open(theargs.outputlistfile, "r")
 outfiles = [line.rstrip('\n') for line in file]
 file.close()
+
 
 #print lines
 '''
@@ -66,13 +80,10 @@ for x in range(0, len(lines)):
 =======
 '''
 def processInput(x):
-    print 'Loading:', (str(lines[x]))
-    #img = lycon.load(lines[x])
+    sys.stdout.write('Loading: ' + str(lines[x]) + '\n')
     img = cv2.imread(lines[x], cv2.IMREAD_UNCHANGED)
-    #type(img)
     cropped = img[in1:in2, in3:in4]
-    print 'Saving:', str(outfiles[x])
-    #lycon.save(outfiles[x], cropped)
+    sys.stdout.write('Saving: ' + str(outfiles[x]) + '\n')
     cv2.imwrite(outfiles[x], cropped)
     return
 
