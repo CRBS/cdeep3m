ebe87502f0abd58bffe70f4b5843d1b5cbbc3760
chris churas <churas@gmail.com>
2018-05-30 00:45:01 +0000

    added os.path.abspath to set absolute path, but this is still failing cause the img object returned is None which causes the cropped line to fail

--- scripts/functions/crop_png.py
+++ scripts/functions/crop_png.py
@@ -1,25 +1,27 @@
 #!/usr/bin/env python
-
 """
 Crop image Frame
 """
 
+import os
 import lycon
 import sys
 #import scipy.io as sio
 #import csv
 from joblib import Parallel, delayed
-
 inputarg = (sys.argv)
+
 print 'Number of arguments:', len(inputarg), 'arguments.'
+
+sys.stdout.write('Args: ' + ' '.join(sys.argv) + '\n')
 in1 = int(inputarg[3])
 in2 = int(inputarg[4])
 in3 = int(inputarg[5])
 in4 = int(inputarg[6])
-print(in1)
-print(in2)
-print(in3)
-print(in4)
+sys.stdout.write('in1 => ' + str(in1) + '\n')
+sys.stdout.write('in2 => ' + str(in2) + '\n')
+sys.stdout.write('in3 => ' + str(in3) + '\n')
+sys.stdout.write('in4 => ' + str(in4) + '\n')
             
 tempmat_infile = str(inputarg[1])
 
@@ -34,9 +36,10 @@ all_outfiles = file.readlines()
 print(len(all_outfiles))
 
 for x in range(0, len(all_infiles)):
-    print 'Loading:', (str(all_infiles[x]))
-    img = lycon.load(str(all_infiles[x])),
-    cropped = img[in1: in2, in3: in4],
-    print 'Saving:', str(all_outfiles[x])
-    lycon.save(str(all_outfiles[x]), cropped),
+    print 'Loading:', os.path.abspath(all_infiles[x])
+    img = lycon.load(os.path.abspath(all_infiles[x]))
+    sys.stdout.write('img => ' + str(img) + '\n')
+    cropped = img[in1:in2, in3:in4]
+    print 'Saving:', os.path.abspath(all_outfiles[x]) + ' with crop '
+    lycon.save(os.path.abspath(all_outfiles[x]), cropped)
     print('done')
